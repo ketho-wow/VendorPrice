a79bd5930c7a231d0d1c6cf877a5f49abae7bff4
Ketho <sghs17@outlook.com>
2019-08-27 05:39:35 +0200

    Added an additional sanity check, refactored code a bit more.

--- VendorPrice.lua
+++ VendorPrice.lua
@@ -1,26 +1,37 @@
 local SELL_PRICE_TEXT = format("%s:", SELL_PRICE)
 
-local function SetPrice(tip)
-    if not MerchantFrame:IsShown() then 
-		local _, itemLink = tip:GetItem()
-		local itemSellPrice = select(11, GetItemInfo(itemLink))
-		if itemSellPrice and itemSellPrice > 0 then -- sellable item
-			local container = GetMouseFocus()
-			local object = container:GetObjectType()
-			local count
-			if object == "Button" then
-				-- ContainerFrameItem, QuestInfoItem, PaperDollItem
-				count = container.count
-			elseif object == "CheckButton" then
-				-- MailItemButton or ActionButton
-				count = container.count or tonumber(container.Count:GetText())
+local function SetGameToolTipPrice(tt)
+	if not MerchantFrame:IsShown() then
+		local itemLink = select(2, tt:GetItem())
+		if itemLink then
+			local itemSellPrice = select(11, GetItemInfo(itemLink))
+			if itemSellPrice and itemSellPrice > 0 then
+				local container = GetMouseFocus()
+				local name = container:GetName()
+				local object = container:GetObjectType()
+				local count
+				if object == "Button" then -- ContainerFrameItem, QuestInfoItem, PaperDollItem
+					count = container.count
+				elseif object == "CheckButton" then -- MailItemButton or ActionButton
+					count = container.count or tonumber(container.Count:GetText())
+				end
+				local cost = (type(count) == "number" and count or 1) * itemSellPrice
+				SetTooltipMoney(tt, cost, nil, SELL_PRICE_TEXT)
 			end
-			local cost = (type(count) == "number" and count or 1) * itemSellPrice
-			SetTooltipMoney(tip, cost, nil, SELL_PRICE_TEXT)
 		end
-    end
+	end
 end
 
-GameTooltip:HookScript("OnTooltipSetItem", SetPrice)
-ItemRefTooltip:HookScript("OnTooltipSetItem", SetPrice)
-print(format("[Vendor Price] ver: (%s) loaded.", GetAddOnMetadata("VendorPrice", "Version")))
+local function SetItemRefToolTipPrice(tt)
+	local itemLink = select(2, tt:GetItem())
+	if itemLink then
+		local itemSellPrice = select(11, GetItemInfo(itemLink))
+		if itemSellPrice and itemSellPrice > 0 then
+			SetTooltipMoney(tt, itemSellPrice, nil, SELL_PRICE_TEXT)
+		end
+	end
+end
+
+GameTooltip:HookScript("OnTooltipSetItem", SetGameToolTipPrice)
+ItemRefTooltip:HookScript("OnTooltipSetItem", SetItemRefToolTipPrice)
+print(format("[Vendor Price] ver: (%s) by Icesythe7 & Ketho loaded.", GetAddOnMetadata("VendorPrice", "Version")))
