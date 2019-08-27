912aa0a8b1133bc89895b2db2cfd62f4c5303613
Ketho <sghs17@outlook.com>
2019-08-27 05:37:52 +0200

    Fixed displaying vendor price on chat links and optimized the code a bit.

--- VendorPrice.lua
+++ VendorPrice.lua
@@ -1,22 +1,18 @@
-local VERSION = "0.04"
-local SELLVALUE_COST = "|cffFFFFFFSell Price:   %s|r"
-local NOTSELLABLE = "This item can not be sold."
+local VERSION = "0.05"
+local SELLVALUE_COST = "|cffFFFFFFSell Price:"
 
 print(format("[Vendor Price] ver: (%s) loaded.", VERSION))
 
-GameTooltip:HookScript("OnTooltipSetItem", function(tip)
-    local _, itemLink = tip:GetItem()
+local function SetPrice(tip)
+	local _, itemLink = tip:GetItem()
     local itemSellPrice = select(11, GetItemInfo(itemLink))
     if itemSellPrice and itemSellPrice > 0 then -- sellable item
-        local count
         local container = GetMouseFocus()
-        if container and container:GetObjectType() == "Button" and container.count then
-            count = container.count
-        end
-        local cost = (count and type(count) == "number" and count > 1) and itemSellPrice * count or itemSellPrice
-        tip:AddLine(format(SELLVALUE_COST, GetMoneyString(cost)))
-    else -- quest/unsellable item
-        tip:AddLine(NOTSELLABLE)
+        local count = container and container:IsObjectType("Button") and container.count
+        local cost = (type(count) == "number" and count or 1) * itemSellPrice
+		SetTooltipMoney(tip, cost, nil, SELLVALUE_COST)
     end
-    tip:Show()
-end)
+end
+
+GameTooltip:HookScript("OnTooltipSetItem", SetPrice)
+ItemRefTooltip:HookScript("OnTooltipSetItem", SetPrice)
