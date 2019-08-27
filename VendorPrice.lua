a3125bd60c5823c3b707d0036fe8d8a4cd77ac5d
Ketho <sghs17@outlook.com>
2019-08-27 05:37:10 +0200

    Fixed issue with items having an enchant.

--- VendorPrice.lua
+++ VendorPrice.lua
@@ -1,5 +1,8 @@
+local VERSION = "0.04"
 local SELLVALUE_COST = "|cffFFFFFFSell Price:   %s|r"
 local NOTSELLABLE = "This item can not be sold."
+
+print(format("[Vendor Price] ver: (%s) loaded.", VERSION))
 
 GameTooltip:HookScript("OnTooltipSetItem", function(tip)
     local _, itemLink = tip:GetItem()
@@ -10,7 +13,7 @@ GameTooltip:HookScript("OnTooltipSetItem", function(tip)
         if container and container:GetObjectType() == "Button" and container.count then
             count = container.count
         end
-        local cost = (count and count > 1) and itemSellPrice * count or itemSellPrice
+        local cost = (count and type(count) == "number" and count > 1) and itemSellPrice * count or itemSellPrice
         tip:AddLine(format(SELLVALUE_COST, GetMoneyString(cost)))
     else -- quest/unsellable item
         tip:AddLine(NOTSELLABLE)
