bd94aee6b9910b694ababf0d470da69f6b9922ba
Ketho <sghs17@outlook.com>
2019-08-27 05:38:30 +0200

    Now properly displays vendor price of stacked items (such as bandages) that are in the actionbar. Moved version information to .toc file (Thanks Ketho)

--- VendorPrice.lua
+++ VendorPrice.lua
@@ -1,16 +1,23 @@
-local VERSION = "0.05"
 local SELLVALUE_COST = "|cffFFFFFFSell Price:"
 
-print(format("[Vendor Price] ver: (%s) loaded.", VERSION))
+print(format("[Vendor Price] ver: (%s) loaded.", GetAddOnMetadata("VendorPrice", "Version")))
 
 local function SetPrice(tip)
-	local _, itemLink = tip:GetItem()
+    local _, itemLink = tip:GetItem()
     local itemSellPrice = select(11, GetItemInfo(itemLink))
     if itemSellPrice and itemSellPrice > 0 then -- sellable item
         local container = GetMouseFocus()
-        local count = container and container:IsObjectType("Button") and container.count
+        local object = container:GetObjectType()
+        local count
+		
+        if object == "Button" then
+            count = container.count
+        elseif object == "CheckButton" then -- ActionButton
+            count = tonumber(container.Count:GetText())
+        end
+        
         local cost = (type(count) == "number" and count or 1) * itemSellPrice
-		SetTooltipMoney(tip, cost, nil, SELLVALUE_COST)
+        SetTooltipMoney(tip, cost, nil, SELLVALUE_COST)
     end
 end
 
