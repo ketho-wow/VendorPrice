2e038bd9510ef53453847243704d60442fdac386
Ketho <sghs17@outlook.com>
2019-08-28 19:46:22 +0200

    Fixed error when using Auctionator
    Fixed prices not being shown for the tradeskill window while at the vendor

--- VendorPrice.lua
+++ VendorPrice.lua
@@ -73,22 +73,25 @@ function f:OnEvent(event, key, state)
 end
 
 local function SetGameToolTipPrice(tt)
-	if not MerchantFrame:IsShown() then
-		local itemLink = select(2, tt:GetItem())
-		if itemLink then
-			local itemSellPrice = select(11, GetItemInfo(itemLink))
-			if itemSellPrice and itemSellPrice > 0 then
-				local container = GetMouseFocus()
-				local name = container:GetName()
-				local object = container:GetObjectType()
-				local count
-				if object == "Button" then -- ContainerFrameItem, QuestInfoItem, PaperDollItem
-					count = container.count
-				elseif object == "CheckButton" then -- MailItemButton or ActionButton
-					count = container.count or tonumber(container.Count:GetText())
+	local container = GetMouseFocus()
+	if container and container.GetName then -- Auctionator sanity check
+		-- price is already shown at vendor; still allow showing price for tradeskill items
+		if not MerchantFrame:IsShown() or container:GetName():find("TradeSkill") then
+			local itemLink = select(2, tt:GetItem())
+			if itemLink then
+				local itemSellPrice = select(11, GetItemInfo(itemLink))
+				if itemSellPrice and itemSellPrice > 0 then
+					local name = container:GetName()
+					local object = container:GetObjectType()
+					local count
+					if object == "Button" then -- ContainerFrameItem, QuestInfoItem, PaperDollItem
+						count = container.count
+					elseif object == "CheckButton" then -- MailItemButton or ActionButton
+						count = container.count or tonumber(container.Count:GetText())
+					end
+					local cost = (type(count) == "number" and count or 1) * itemSellPrice
+					SetTooltipMoney(tt, cost, nil, SELL_PRICE_TEXT)
 				end
-				local cost = (type(count) == "number" and count or 1) * itemSellPrice
-				SetTooltipMoney(tt, cost, nil, SELL_PRICE_TEXT)
 			end
 		end
 	end
