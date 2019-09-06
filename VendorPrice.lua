b9dad51a6f31ada9d2819f6d9b928693167c6beb
Ketho <sghs17@outlook.com>
2019-09-06 22:12:27 +0200

    Added shift modifier for price per unit

--- VendorPrice.lua
+++ VendorPrice.lua
@@ -4,18 +4,24 @@ local function ShouldShowPrice(tt)
 	if MerchantFrame:IsShown() then
 		local name = tt:GetOwner():GetName()
 		return name:find("Character") or name:find("TradeSkill")
-	else
-		return true
 	end
+	return true
 end
 
 local function SetPrice(tt, count, item)
 	if ShouldShowPrice(tt) then
 		item = item or select(2, tt:GetItem())
 		if item then
-			local money = select(11, GetItemInfo(item)) * count
+			local sellPrice = select(11, GetItemInfo(item))
+			local money = sellPrice * count
 			if money > 0 then
-				SetTooltipMoney(tt, money, nil, SELL_PRICE_TEXT)
+				if IsShiftKeyDown() and count > 1 then
+					local align = count >= 10 and "  " or ""
+					SetTooltipMoney(tt, sellPrice, nil, SELL_PRICE_TEXT.." |cffAAAAFFx1|r"..align)
+					SetTooltipMoney(tt, money, nil, SELL_PRICE_TEXT..format(" |cffAAAAFFx%d|r", count))
+				else
+					SetTooltipMoney(tt, money, nil, SELL_PRICE_TEXT)
+				end
 				tt:Show()
 			end
 		end
@@ -45,7 +51,8 @@ local SetItem = {
 	--SetCompareItem
 	SetCraftItem = function(tt, index, reagent)
 		local _, _, count = GetCraftReagentInfo(index, reagent)
-		local itemLink = GetCraftReagentItemLink(index, reagent) -- tt:GetItem() returns an empty link
+		 -- otherwise returns an empty link
+		local itemLink = GetCraftReagentItemLink(index, reagent)
 		SetPrice(tt, count, itemLink)
 	end,
 	SetInboxItem = function(tt, messageIndex, attachIndex)
@@ -108,37 +115,39 @@ for name, func in pairs(SetItem) do
 	hooksecurefunc(GameTooltip, name, func)
 end
 
+-- item information is not readily available on tt:SetHyperlink()
 ItemRefTooltip:HookScript("OnTooltipSetItem", function(tt)
 	local item = select(2, tt:GetItem())
 	if item then
 		local itemSellPrice = select(11, GetItemInfo(item))
 		if itemSellPrice and itemSellPrice > 0 then
-			-- recipes show the price twice when hooking OnTooltipSetItem
-			tt.shownMoneyFrames = nil
+			tt.shownMoneyFrames = nil -- OnTooltipSetItem fires twice for recipes
 			SetTooltipMoney(tt, itemSellPrice, nil, SELL_PRICE_TEXT)
 		end
 	end
 end)
 
-local function OnTooltipSetItem(tt)
-	if BagnonFramebank and BagnonFramebank:IsMouseOver() then
-		local info = tt:GetOwner():GetParent().info
-		if info then
-			tt.shownMoneyFrames = nil
-			SetPrice(tt, info.count or 1)
-		end
-	end
-end
-
 local function OnEvent(self, event, isInitialLogin, isReloadingUi)
 	if isInitialLogin or isReloadingUi then
 		-- support bagnon /bank
 		if Bagnon then
-			GameTooltip:HookScript("OnTooltipSetItem", OnTooltipSetItem)
+			GameTooltip:HookScript("OnTooltipSetItem", function(tt)
+				if BagnonFramebank and BagnonFramebank:IsMouseOver() then
+					local info = tt:GetOwner():GetParent().info
+					if info then
+						tt.shownMoneyFrames = nil
+						SetPrice(tt, info.count or 1)
+					end
+				end
+			end)
 		end
-		-- disable Auctionator vendor price
-		if Auctionator and AUCTIONATOR_V_TIPS == 1 then
-			AUCTIONATOR_V_TIPS = 0
+		-- disable Auctionator vendor price in tooltips
+		if Auctionator then
+			if AUCTIONATOR_V_TIPS == 1 then
+				AUCTIONATOR_V_TIPS = 0
+			end
+			local option = ATR_tipsVendorOpt_CB_Text
+			option:SetText(option:GetText().." |cffFFFF00(Disabled by Vendor Price)|r")
 		end
 	end
 end
